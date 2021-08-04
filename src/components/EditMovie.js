0924aa89bd7c50cd4306be498d3693db294bd779
weilyuwang <weilyuwang@gmail.com>
2021-08-04 00:11:38 -0400

    delete movie

--- src/components/EditMovie.js
+++ src/components/EditMovie.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-
+import { useParams, Link, useHistory } from "react-router-dom";
 import Input from "./form-components/Input";
 import TextArea from "./form-components/TextArea";
 import Select from "./form-components/Select";
 import Alert from "./ui-components/Alert";
 import "./EditMovie.css";
+import { confirmAlert } from "react-confirm-alert";
+import "react-confirm-alert/src/react-confirm-alert.css";
 
 const mpaaOptions = [
   { id: "G", value: "G" },
@@ -17,6 +18,7 @@ const mpaaOptions = [
 
 const EditMovie = () => {
   const { id } = useParams();
+  const history = useHistory();
 
   const [movie, setMovie] = useState({});
   const [isLoaded, setIsLoaded] = useState(false);
@@ -120,86 +122,129 @@ const EditMovie = () => {
     return errors.indexOf(key) !== -1;
   };
 
-  if (isLoaded) {
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else {
-      return (
-        <div className="container">
-          <h2>Add/Edit Movie</h2>
-          <Alert alertType={alertType} alertMessage={alertMessage} />
-          <hr />
-          <form onSubmit={handleSubmit}>
-            <input
-              type="hidden"
-              name="id"
-              id="id"
-              value={movie.id}
-              onChange={handleChange}
-            />
-
-            <Input
-              label="Title"
-              className={hasError("title") ? "is-invalid" : ""}
-              name="title"
-              type="text"
-              value={movie.title}
-              handleChange={handleChange}
-              errorDiv={hasError("title") ? "text-danger" : "d-none"}
-              errorMessage={"Please enter a title"}
-            />
-
-            <Input
-              label="Release Date"
-              name="release_date"
-              type="date"
-              value={movie.release_date}
-              handleChange={handleChange}
-              placeholder="yyyy-mm-dd"
-            />
-
-            <Input
-              label="Runtime"
-              name="runtime"
-              type="text"
-              value={movie.runtime}
-              handleChange={handleChange}
-            />
-
-            <Select
-              name="mpaa_rating"
-              label="MPAA Rating"
-              value={movie.mpaa_rating}
-              handleChange={handleChange}
-              options={mpaaOptions}
-              placeholder="Choose..."
-            />
-
-            <Input
-              label="Rating"
-              name="rating"
-              type="text"
-              value={movie.rating}
-              handleChange={handleChange}
-            />
-
-            <TextArea
-              label="Description"
-              name="description"
-              value={movie.description}
-              rows="3"
-              handleChange={handleChange}
-            />
-
-            <hr />
-
-            <button className="btn btn-primary">Save</button>
-          </form>
-        </div>
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(
+        "http://localhost:4000/v1/admin/deletemovie/" + movie.id,
+        {
+          method: "GET",
+        }
       );
+      const data = await response.json();
+      if (data.error) {
+        setAlertType("alert-danger");
+        setAlertMessage(data.error.message);
+      } else {
+        history.push({ pathname: "/admin" });
+      }
+    } catch (err) {
+      setError(err);
     }
-  } else {
+  };
+
+  const confirmDelete = () => {
+    confirmAlert({
+      title: "Delete Movie?",
+      message: "Are you sure?",
+      buttons: [
+        {
+          label: "Yes",
+          onClick: () => handleDelete(),
+        },
+        {
+          label: "No",
+          onClick: () => {},
+        },
+      ],
+    });
+  };
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
     return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="container">
+        <h2>Add/Edit Movie</h2>
+        <Alert alertType={alertType} alertMessage={alertMessage} />
+        <hr />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="hidden"
+            name="id"
+            id="id"
+            value={movie.id}
+            onChange={handleChange}
+          />
+
+          <Input
+            label="Title"
+            className={hasError("title") ? "is-invalid" : ""}
+            name="title"
+            type="text"
+            value={movie.title}
+            handleChange={handleChange}
+            errorDiv={hasError("title") ? "text-danger" : "d-none"}
+            errorMessage={"Please enter a title"}
+          />
+
+          <Input
+            label="Release Date"
+            name="release_date"
+            type="date"
+            value={movie.release_date}
+            handleChange={handleChange}
+            placeholder="yyyy-mm-dd"
+          />
+
+          <Input
+            label="Runtime"
+            name="runtime"
+            type="text"
+            value={movie.runtime}
+            handleChange={handleChange}
+          />
+
+          <Select
+            name="mpaa_rating"
+            label="MPAA Rating"
+            value={movie.mpaa_rating}
+            handleChange={handleChange}
+            options={mpaaOptions}
+            placeholder="Choose..."
+          />
+
+          <Input
+            label="Rating"
+            name="rating"
+            type="text"
+            value={movie.rating}
+            handleChange={handleChange}
+          />
+
+          <TextArea
+            label="Description"
+            name="description"
+            value={movie.description}
+            rows="3"
+            handleChange={handleChange}
+          />
+
+          <hr />
+
+          <button className="btn btn-primary">Save</button>
+          <Link to="/admin" className="btn btn-warning ms-1">
+            Cancel
+          </Link>
+          {movie.id > 0 && (
+            <span onClick={confirmDelete} className="btn btn-danger ms-1">
+              Delete
+            </span>
+          )}
+        </form>
+      </div>
+    );
   }
 };
 
