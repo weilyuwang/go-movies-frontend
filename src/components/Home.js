3d5857815906e23f20731d892138b008c813b1e4
weilyuwang <weilyuwang@gmail.com>
2021-08-09 22:52:50 -0400

    add image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Ticket from "./../images/movie_tickets.jpg";
+import "./Home.css";
 
 const Home = () => {
-  return <h2>Home</h2>;
+  return (
+    <div className="text-center">
+      <h2>This is the home page</h2>
+      <hr />
+      <img src={Ticket} alt="movie ticket" />
+      {/* <hr />
+      <div className="ticket"></div> */}
+    </div>
+  );
 };
 
 export default Home;
